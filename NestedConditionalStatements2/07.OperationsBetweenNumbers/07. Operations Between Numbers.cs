7586c091c86b47377750cdadaba169e6f785f766
Anton Petrov <toniivanov03@gmail.com>
2019-06-15 12:20:38 +0300

    Seventh to tenth problem

---
+++ NestedConditionalStatements2/07.OperationsBetweenNumbers/07. Operations Between Numbers.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace _07.OperationsBetweenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            char sign = char.Parse(Console.ReadLine());
+
+            string evenOrOdd = string.Empty;
+
+            if (sign == '+')
+            {
+                if ((num1 + num2) % 2 == 0)
+                {
+                    evenOrOdd = "even";
+                }
+                else
+                {
+                    evenOrOdd = "odd";
+                }
+                Console.WriteLine($"{num1} + {num2} = {num1 + num2} - {evenOrOdd}");
+            }
+            else if (sign == '-')
+            {
+                if ((num1 - num2) % 2 == 0)
+                {
+                    evenOrOdd = "even";
+                }
+                else
+                {
+                    evenOrOdd = "odd";
+                }
+                Console.WriteLine($"{num1} - {num2} = {num1 - num2} - {evenOrOdd}");
+            }
+            else if (sign == '*')
+            {
+                if (num1 * num2 % 2 == 0)
+                {
+                    evenOrOdd = "even";
+                }
+                else
+                {
+                    evenOrOdd = "odd";
+                }
+                Console.WriteLine($"{num1} * {num2} = {num1 * num2} - {evenOrOdd}");
+            }
+            else if (sign == '/')
+            {
+                if (num2 == 0)
+                {
+                    Console.WriteLine($"Cannot divide {num1} by zero");
+                }
+                else
+                {
+                    Console.WriteLine($"{num1} / {num2} = {num1 / num2:F2}");
+                }
+            }
+            else if (sign == '%')
+            {
+                if (num2 == 0)
+                {
+                    Console.WriteLine($"Cannot divide {num1} by zero");
+                }
+                else
+                {
+                    Console.WriteLine($"{num1} % {num2} = {num1 % num2}");
+                }
+            }
+        }
+    }
+}
---
+++ NestedConditionalStatements2/08.HotelRoom/08.HotelRoom.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace _08.HotelRoom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = Console.ReadLine();
+            int numNights = int.Parse(Console.ReadLine());
+
+            double studio = 0;
+            double apartment = 0;
+            const double nightMayOrOctStudio = 50;
+            const double nightMayOrOctApartment = 65;
+            const double nightJuneOrSepStudio = 75.2;
+            const double nightJuneOrSepApartment = 68.7;
+            const double nightJulyOrAugustStudio = 76;
+            const double nightJulyOrAugustApartment = 77;
+
+            if (month == "May" || month == "October")
+            {
+                studio = nightMayOrOctStudio * numNights;
+                apartment = nightMayOrOctApartment * numNights;
+
+                if (numNights > 7 && numNights < 14)
+                {
+                    studio -= studio * 0.05;
+                }
+                else if (numNights > 14)
+                {
+                    studio -= studio * 0.3;
+                    apartment -= apartment * 0.1;
+                }
+            }
+            else if (month == "June" || month == "September")
+            {
+                studio = nightJuneOrSepStudio * numNights;
+                apartment = nightJuneOrSepApartment * numNights;
+
+                if (numNights > 14)
+                {
+                    studio -= studio * 0.2;
+                    apartment -= apartment * 0.1;
+                }
+            }
+            else if (month == "July" || month == "August")
+            {
+                studio = nightJulyOrAugustStudio * numNights;
+                apartment = nightJulyOrAugustApartment * numNights;
+
+                if (numNights > 14)
+                {
+                    apartment -= apartment * 0.1;
+                }
+            }
+            Console.WriteLine($"Apartment: {apartment:F2} lv.");
+            Console.WriteLine($"Studio: {studio:F2} lv.");
+        }
+    }
+}
