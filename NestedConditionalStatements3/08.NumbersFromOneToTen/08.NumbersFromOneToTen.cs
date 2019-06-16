bf30ee053ceeb27660cf841ff176b732500c1c12
Anton Petrov <toniivanov03@gmail.com>
2019-06-16 19:49:33 +0300

    Numbers from 1 to 10

---
+++ NestedConditionalStatements3/08.NumbersFromOneToTen/08.NumbersFromOneToTen.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _08.NumbersFromOneToTen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+
+            while (num <= 10)
+            {
+                Console.WriteLine(num);
+                num++;
+            }
+        }
+    }
+}
