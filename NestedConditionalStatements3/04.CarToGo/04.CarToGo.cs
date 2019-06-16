f7c6556a2fb575d2c738771dffa4e1f1491422cf
Anton Petrov <toniivanov03@gmail.com>
2019-06-16 16:22:38 +0300

    Car to go

---
+++ NestedConditionalStatements3/04.CarToGo/04.CarToGo.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace _04.CarToGo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+            string summerOrWinter = Console.ReadLine();
+
+            string Class = string.Empty;
+            string car = string.Empty;
+            double price = 0;
+
+            if (budget > 0 && budget <= 100)
+            {
+                Class = "Economy class";
+
+                if (summerOrWinter == "Summer")
+                {
+                    car = "Cabrio";
+                    price = budget * 0.35;
+                }
+                else
+                {
+                    car = "Jeep";
+                    price = budget * 0.65;
+                }
+            }
+            else if (budget > 100 && budget <= 500)
+            {
+                Class = "Compact class";
+
+                if (summerOrWinter == "Summer")
+                {
+                    car = "Cabrio";
+                    price = budget * 0.45;
+                }
+                else
+                {
+                    car = "Jeep";
+                    price = budget * 0.8;
+                }
+            }
+            else if (budget > 500)
+            {
+                Class = "Luxury class";
+                car = "Jeep";
+                price = budget * 0.9;
+            }
+
+            Console.WriteLine(Class);
+            Console.WriteLine($"{car} - {price:F2}");
+        }
+    }
+}
