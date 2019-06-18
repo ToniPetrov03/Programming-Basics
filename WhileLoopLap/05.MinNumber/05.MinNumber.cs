11054d4911b5a8702d47eff898966915f1d201c7
Anton Petrov <toniivanov03@gmail.com>
2019-06-18 21:08:22 +0300

    Min number

---
+++ WhileLoopLap/05.MinNumber/05.MinNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _04.MaxNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numNumbers = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+            int smallestNumber = int.MaxValue;
+
+            while (numNumbers > counter)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (num < smallestNumber)
+                {
+                    smallestNumber = num;
+                }
+
+                counter++;
+            }
+
+            Console.WriteLine(smallestNumber);
+        }
+    }
+}
