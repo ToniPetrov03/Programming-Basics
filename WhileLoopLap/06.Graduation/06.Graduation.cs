e1392495303b066e0c1e0d7caebb06771b061b6a
Anton Petrov <toniivanov03@gmail.com>
2019-06-19 13:21:36 +0300

    Graduation

---
+++ WhileLoopLap/06.Graduation/06.Graduation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _06.Graduation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+
+            int counter = 0;
+            double sumMarks = 0;
+
+            while (counter < 12)
+            {
+                double mark = double.Parse(Console.ReadLine());
+
+                if (mark >= 4)
+                {
+                    sumMarks += mark;
+                    counter++;
+                }
+            }
+
+            double average = sumMarks / 12;
+
+            Console.WriteLine($"{name} graduated. Average grade: {average:F2}");
+        }
+    }
+}
