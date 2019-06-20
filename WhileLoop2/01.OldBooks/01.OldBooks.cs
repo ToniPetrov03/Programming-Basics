8ce11a759b9251e9b685891b59894b6f3e16b58d
Anton Petrov <toniivanov03@gmail.com>
2019-06-20 19:05:06 +0300

    Old books

---
+++ WhileLoop2/01.OldBooks/01.OldBooks.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _01.OldBooks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string searchableBook = Console.ReadLine();
+            int booksInLibrary = int.Parse(Console.ReadLine());
+
+            string book = string.Empty;
+            int counter = 0;
+
+            while (counter < booksInLibrary)
+            {
+                book = Console.ReadLine();
+
+                if (book == searchableBook)
+                {
+                    Console.WriteLine($"You checked {counter} books and found it.");
+                    break;
+                }
+
+                counter++;
+            }
+
+            if (book != searchableBook)
+            {
+                Console.WriteLine("The book you search is not here!");
+                Console.WriteLine($"You checked {counter} books.");
+            }
+        }
+    }
+}
