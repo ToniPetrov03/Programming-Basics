45dd6519800f92d016d0447837105e6593cbd3e3
Anton Petrov <toniivanov03@gmail.com>
2019-06-21 17:19:17 +0300

    Vacation

---
+++ WhileLoop2/03.Vacation/03.Vacation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _03.Vacation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double neededMoney = double.Parse(Console.ReadLine());
+            double moneyOnHand = double.Parse(Console.ReadLine());
+
+            int spendCounter = 0;
+            int daysCounter = 0;
+
+            while (moneyOnHand < neededMoney && spendCounter < 5)
+            {
+                string spendOrSave = Console.ReadLine();
+                double money = double.Parse(Console.ReadLine());
+
+                if (spendOrSave == "spend")
+                {
+                    if (moneyOnHand - money < 0)
+                    {
+                        moneyOnHand = 0;
+                    }
+                    else
+                    {
+                        moneyOnHand -= money;
+                    }
+
+                    spendCounter++;
+                }
+                else
+                {
+                    moneyOnHand += money;
+
+                    spendCounter = 0;
+                }
+
+                daysCounter++;
+            }
+
+            if (spendCounter == 5)
+            {
+                Console.WriteLine($"You can't save the money.");
+                Console.WriteLine(daysCounter);
+            }
+            else if (moneyOnHand >= neededMoney)
+            {
+                Console.WriteLine($"You saved the money for {daysCounter} days.");
+            }
+        }
+    }
+}
