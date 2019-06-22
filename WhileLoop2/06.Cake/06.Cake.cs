f060cb3496a6d7cba260613bb589a2f5f099c0ce
Anton Petrov <toniivanov03@gmail.com>
2019-06-22 11:35:09 +0300

    Cake

---
+++ WhileLoop2/06.Cake/06.Cake.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _06.Cake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+
+            int sizeOfCake = width * length;
+
+            string stopOrPieceSize = string.Empty;
+            int pieceSize;
+            int takenSizeOfCake = 0;
+
+            while (takenSizeOfCake <= sizeOfCake)
+            {
+                stopOrPieceSize = Console.ReadLine();
+
+                if (stopOrPieceSize == "STOP")
+                {
+                    Console.WriteLine($"{sizeOfCake - takenSizeOfCake} pieces are left.");
+                    break;
+                }
+                else
+                {
+                    pieceSize = int.Parse(stopOrPieceSize);
+                }
+
+                takenSizeOfCake += pieceSize;
+            }
+
+            if (takenSizeOfCake > sizeOfCake)
+            {
+                Console.WriteLine($"No more cake left! You need {takenSizeOfCake - sizeOfCake} pieces more.");
+            }
+        }
+    }
+}
