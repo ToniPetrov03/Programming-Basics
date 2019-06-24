c1ac193b97ee101462e1050050a65e540be08584
ToniPetrov03 <35171489+ToniPetrov03@users.noreply.github.com>
2019-06-24 23:34:46 +0300

    While-Loop - More Exercises (#9)

---
+++ WhileLoop3/05.AverageNumber/05.AverageNumber.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _05.AverageNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfNumber = int.Parse(Console.ReadLine());
+            double sum = 0;
+
+            for (int i = 0; i < numOfNumber; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                sum += num;
+            }
+
+            Console.WriteLine("{0:F2}", sum / numOfNumber);
+        }
+    }
+}
