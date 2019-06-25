3f5b3b7cc2b24e2d421fffd6d321ccdabbce5872
Anton Petrov <toniivanov03@gmail.com>
2019-06-25 08:19:38 +0300

    Left and right sum

---
+++ For-Loop/05.LeftAndRightSum/05.LeftAndRightSum.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _05.LeftAndRightSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int numNumbers = 2 * n;
+            int leftSum = 0;
+            int rightSum = 0;
+
+            for (int i = 0; i < numNumbers; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+
+                if (i < n)
+                {
+                    leftSum += num;
+                }
+                else
+                {
+                    rightSum += num;
+                }
+            }
+
+            if (leftSum == rightSum)
+            {
+                Console.WriteLine($"Yes, sum = {leftSum}");
+            }
+            else
+            {
+                Console.WriteLine($"No, diff = {Math.Abs(leftSum - rightSum)}");
+            }
+        }
+    }
+}
