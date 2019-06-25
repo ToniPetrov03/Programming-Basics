ded8c9646e60284df9a97c2a6f7557a4a11727a1
Anton Petrov <toniivanov03@gmail.com>
2019-06-25 23:45:46 +0300

    Correction of fifth problem

--- For-Loop/05.LeftAndRightSum/05.LeftAndRightSum.cs
+++ For-Loop/05.LeftAndRightSum/05.LeftAndRightSum.cs
@@ -4,27 +4,24 @@ namespace _05.LeftAndRightSum
 {
     class Program
     {
+        private static int SumUserInput(int n)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                sum += num;
+            }
+
+            return sum;
+        }
         static void Main(string[] args)
         {
             int n = int.Parse(Console.ReadLine());
 
-            int numNumbers = 2 * n;
-            int leftSum = 0;
-            int rightSum = 0;
-
-            for (int i = 0; i < numNumbers; i++)
-            {
-                int num = int.Parse(Console.ReadLine());
-
-                if (i < n)
-                {
-                    leftSum += num;
-                }
-                else
-                {
-                    rightSum += num;
-                }
-            }
+            int leftSum = SumUserInput(n);
+            int rightSum = SumUserInput(n);
 
             if (leftSum == rightSum)
             {
