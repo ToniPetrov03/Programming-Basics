4e94b215f361466f0d9dd7872ccacae1842faea5
Anton Petrov <toniivanov03@gmail.com>
2019-06-29 20:35:44 +0300

    Corrected problem 4

--- For-Loop2/04.EqualPairs/04.EqualPairs.cs
+++ For-Loop2/04.EqualPairs/04.EqualPairs.cs
@@ -8,39 +8,33 @@ namespace _04.EqualPairs
         {
             int n = int.Parse(Console.ReadLine());
 
-            int counter = 0;
-            int firstPairSum = 0;
-            int secondPairSum = 0;
             int maxDiff = 0;
+            int diff = 0;
+            int oldSum = 0;
 
             for (int i = 0; i < n; i++)
             {
                 int num = int.Parse(Console.ReadLine());
                 int num2 = int.Parse(Console.ReadLine());
 
-                if (counter == 0)
-                {
-                    firstPairSum = num + num2;
-                }
-                else if (counter == 1)
-                {
-                    secondPairSum = num + num2;
+                int sum = num + num2;
 
-                    if (Math.Abs(firstPairSum - secondPairSum) > maxDiff)
-                    {
-                        maxDiff = Math.Abs(firstPairSum - secondPairSum);
-                    }
-
-                    firstPairSum = secondPairSum;
-                    counter = 0;
+                if (i > 0)
+                {
+                    diff = Math.Abs(sum - oldSum);
                 }
 
-                counter++;
+                if (maxDiff < diff)
+                {
+                    maxDiff = diff;
+                }
+
+                oldSum = sum;
             }
 
             if (maxDiff == 0)
             {
-                Console.WriteLine($"Yes, value={firstPairSum}");
+                Console.WriteLine($"Yes, value={oldSum}");
             }
             else
             {
