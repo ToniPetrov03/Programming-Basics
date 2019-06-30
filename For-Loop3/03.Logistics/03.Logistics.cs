6321b02b34c109d2954770123985c8ca42fd14e9
Anton Petrov <toniivanov03@gmail.com>
2019-07-01 00:28:17 +0300

    Logistics (improved)

--- For-Loop3/03.Logistics/03.Logistics.cs
+++ For-Loop3/03.Logistics/03.Logistics.cs
@@ -8,14 +8,15 @@ namespace _03.Logistics
         {
             int numGoods = int.Parse(Console.ReadLine());
 
-            const int moneyForTonTo3tons = 200;
-            const int moneyForTonTo11tons = 175;
-            const int moneyForTonMoreThan11tons = 120;
-            double totalTons = 0d;
+            const int moneyForTonTo3Tons = 200;
+            const int moneyForTonTo11Tons = 175;
+            const int moneyForTonMoreThan11Tons = 120;
+
             int money = 0;
-            int totalTonsTo3tons = 0;
-            int totalTonsTo11tons = 0;
-            int totalTonsMoreThan11tons = 0;
+            double totalTons = 0d;
+            int totalTonsTo3Tons = 0;
+            int totalTonsTo11Tons = 0;
+            int totalTonsMoreThan11Tons = 0;
 
             for (int i = 0; i < numGoods; i++)
             {
@@ -23,27 +24,27 @@ namespace _03.Logistics
 
                 if (tons <= 3)
                 {
-                    money += tons * moneyForTonTo3tons;
-                    totalTonsTo3tons += tons;
+                    money += tons * moneyForTonTo3Tons;
+                    totalTonsTo3Tons += tons;
                 }
                 else if (tons <= 11)
                 {
-                    money += tons * moneyForTonTo11tons;
-                    totalTonsTo11tons += tons;
+                    money += tons * moneyForTonTo11Tons;
+                    totalTonsTo11Tons += tons;
                 }
-                else if (tons > 11)
+                else
                 {
-                    money += tons * moneyForTonMoreThan11tons;
-                    totalTonsMoreThan11tons += tons;
+                    money += tons * moneyForTonMoreThan11Tons;
+                    totalTonsMoreThan11Tons += tons;
                 }
 
                 totalTons += tons;
             }
 
             Console.WriteLine($"{money / totalTons:F2}");
-            Console.WriteLine($"{totalTonsTo3tons / totalTons * 100:F2}%");
-            Console.WriteLine($"{totalTonsTo11tons / totalTons * 100:F2}%");
-            Console.WriteLine($"{totalTonsMoreThan11tons / totalTons * 100:F2}%");
+            Console.WriteLine($"{totalTonsTo3Tons / totalTons * 100:F2}%");
+            Console.WriteLine($"{totalTonsTo11Tons / totalTons * 100:F2}%");
+            Console.WriteLine($"{totalTonsMoreThan11Tons / totalTons * 100:F2}%");
         }
     }
 }
