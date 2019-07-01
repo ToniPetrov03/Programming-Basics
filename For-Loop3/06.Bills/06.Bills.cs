49b5f852cced1b4a7f74ddd3f0cc6735afc9fe37
Anton Petrov <toniivanov03@gmail.com>
2019-07-01 13:07:30 +0300

    Bills

---
+++ For-Loop3/06.Bills/06.Bills.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _06.Bills
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int months = int.Parse(Console.ReadLine());
+
+            const int waterBill = 20;
+            const int internetBill = 15;
+
+            double totalelectricityBill = 0;
+            double totalWaterBill = 0;
+            double totalInternetBill = 0;
+            double totalOtherBill = 0;
+
+
+            for (int i = 0; i < months; i++)
+            {
+                double electricityBill = double.Parse(Console.ReadLine());
+
+                totalelectricityBill += electricityBill;
+                totalWaterBill += waterBill;
+                totalInternetBill += internetBill;
+                totalOtherBill += (electricityBill + waterBill + internetBill) * 1.2;
+            }
+
+            Console.WriteLine($"Electricity: {totalelectricityBill:F2} lv");
+            Console.WriteLine($"Water: {totalWaterBill:F2} lv");
+            Console.WriteLine($"Internet: {totalInternetBill:F2} lv");
+            Console.WriteLine($"Other: {totalOtherBill:F2} lv");
+            Console.WriteLine($"Average: {(totalelectricityBill + totalWaterBill + totalInternetBill + totalOtherBill) / months:F2} lv");
+        }
+    }
+}
