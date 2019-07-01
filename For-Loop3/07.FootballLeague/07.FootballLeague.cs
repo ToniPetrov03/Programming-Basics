597a6ed25e7443f96fab7d52598bd383893f4718
Anton Petrov <toniivanov03@gmail.com>
2019-07-01 13:24:32 +0300

    Football league

---
+++ For-Loop3/07.FootballLeague/07.FootballLeague.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _07.FootballLeague
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int capacityOfStadium = int.Parse(Console.ReadLine());
+            double numFans = double.Parse(Console.ReadLine());
+
+            double fansOfSectorA = 0;
+            double fansOfSectorB = 0;
+            double fansOfSectorV = 0;
+            double fansOfSectorG = 0;
+
+            for (int i = 0; i < numFans; i++)
+            {
+                char sector = char.Parse(Console.ReadLine());
+
+                switch (sector)
+                {
+                    case 'A': fansOfSectorA++; break;
+                    case 'B': fansOfSectorB++; break;
+                    case 'V': fansOfSectorV++; break;
+                    case 'G': fansOfSectorG++; break;
+                }
+            }
+
+            Console.WriteLine($"{fansOfSectorA / numFans * 100:F2}%");
+            Console.WriteLine($"{fansOfSectorB / numFans * 100:F2}%");
+            Console.WriteLine($"{fansOfSectorV / numFans * 100:F2}%");
+            Console.WriteLine($"{fansOfSectorG / numFans * 100:F2}%");
+            Console.WriteLine($"{numFans / capacityOfStadium * 100:F2}%");
+        }
+    }
+}
