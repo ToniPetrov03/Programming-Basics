b7e7c9c0682ebdcb5da4bfd76fb0625b2f50e30b
Anton Petrov <toniivanov03@gmail.com>
2019-07-01 13:37:18 +0300

    Clock - part 2

---
+++ For-Loop3/09.ClockPart2/09.ClockPart2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _09.ClockPart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int hour = 0; hour < 24; hour++)
+            {
+                for (int minutes = 0; minutes < 60; minutes++)
+                {
+                    for (int seconds = 0; seconds < 60; seconds++)
+                    {
+                        Console.WriteLine($"{hour} : {minutes} : {seconds}");
+                    }
+                }
+            }
+        }
+    }
+}
