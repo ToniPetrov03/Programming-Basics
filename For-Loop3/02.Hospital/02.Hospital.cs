5bba17970135841d42fabc3bfe220968ea70886f
ToniPetrov03 <35171489+ToniPetrov03@users.noreply.github.com>
2019-07-02 19:35:30 +0300

    For-Loop - More Exercises (#13)

---
+++ For-Loop3/02.Hospital/02.Hospital.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _02.Hospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int period = int.Parse(Console.ReadLine());
+
+            int doctors = 7;
+            int treatedPatients = 0;
+            int untreatedPatients = 0;
+
+            for (int days = 1; days <= period; days++)
+            {
+                int patients = int.Parse(Console.ReadLine());
+
+                if (days % 3 == 0)
+                {
+                    if (untreatedPatients > treatedPatients)
+                    {
+                        doctors++;
+                    }
+                }
+
+                if (patients < doctors)
+                {
+                    treatedPatients += patients;
+                    continue;
+                }
+
+                treatedPatients += doctors;
+                patients -= doctors;
+                untreatedPatients += patients;
+            }
+
+            Console.WriteLine($"Treated patients: {treatedPatients}.");
+            Console.WriteLine($"Untreated patients: {untreatedPatients}.");
+        }
+    }
+}
---
+++ For-Loop3/03.Logistics/03.Logistics.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _03.Logistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numGoods = int.Parse(Console.ReadLine());
+
+            const int moneyForTonTo3Tons = 200;
+            const int moneyForTonTo11Tons = 175;
+            const int moneyForTonMoreThan11Tons = 120;
+
+            int money = 0;
+            double totalTons = 0d;
+            int totalTonsTo3Tons = 0;
+            int totalTonsTo11Tons = 0;
+            int totalTonsMoreThan11Tons = 0;
+
+            for (int i = 0; i < numGoods; i++)
+            {
+                int tons = int.Parse(Console.ReadLine());
+
+                if (tons <= 3)
+                {
+                    money += tons * moneyForTonTo3Tons;
+                    totalTonsTo3Tons += tons;
+                }
+                else if (tons <= 11)
+                {
+                    money += tons * moneyForTonTo11Tons;
+                    totalTonsTo11Tons += tons;
+                }
+                else
+                {
+                    money += tons * moneyForTonMoreThan11Tons;
+                    totalTonsMoreThan11Tons += tons;
+                }
+
+                totalTons += tons;
+            }
+
+            Console.WriteLine($"{money / totalTons:F2}");
+            Console.WriteLine($"{totalTonsTo3Tons / totalTons * 100:F2}%");
+            Console.WriteLine($"{totalTonsTo11Tons / totalTons * 100:F2}%");
+            Console.WriteLine($"{totalTonsMoreThan11Tons / totalTons * 100:F2}%");
+        }
+    }
+}
