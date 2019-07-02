d0a653720d04f64c36188347922db6f2ae07686b
Anton Petrov <toniivanov03@gmail.com>
2019-07-02 12:36:19 +0300

    Cookie factory

---
+++ NestedLoops/08.CookieFactory/08.CookieFactory.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _08.CookieFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numBatches = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numBatches; i++)
+            {
+                bool containsEggs = false;
+                bool containsFlour = false;
+                bool containsSugar = false;
+
+                while (true)
+                {
+                    string ingredient = Console.ReadLine();
+
+                    switch (ingredient)
+                    {
+                        case "eggs": containsEggs = true; break;
+                        case "flour": containsFlour = true; break;
+                        case "sugar": containsSugar = true; break;
+                    }
+
+                    if (ingredient == "Bake!")
+                    {
+                        if (containsEggs && containsFlour && containsSugar)
+                        {
+                            Console.WriteLine($"Baking batch number {i}...");
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"The batter should contain flour, eggs and sugar!");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
