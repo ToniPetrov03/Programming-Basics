cb4054fa52d56433fd6ae610d772380470260dc6
Anton Petrov <toniivanov03@gmail.com>
2019-07-18 13:45:45 +0300

    Coding

---
+++ NestedLoops2/03.Coding/03.Coding.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _03.Coding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (; num != 0; num /= 10)
+            {
+                int n = num % 10;
+
+                if (n == 0)
+                {
+                    Console.WriteLine("ZERO");
+                    continue;
+                }
+
+                char symbol = (char)(n + 33);
+
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(symbol);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
