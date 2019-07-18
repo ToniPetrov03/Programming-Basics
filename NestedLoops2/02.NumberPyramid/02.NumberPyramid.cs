81e3e555e4f9eeed4d53afffc8210ffa442ce964
Anton Petrov <toniivanov03@gmail.com>
2019-07-18 12:28:19 +0300

    Number pyramid

---
+++ NestedLoops2/02.NumberPyramid/02.NumberPyramid.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _02.NumberPyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            int numbersOnRow = 1;
+
+            for (int i = 1; i <= num;)
+            {
+                for (int j = 1; j <= numbersOnRow; j++)
+                {
+                    if (i > num)
+                    {
+                        break;
+                    }
+
+                    Console.Write(i + " ");
+                    i++;
+                }
+
+                Console.WriteLine();
+                numbersOnRow++;
+            }
+        }
+    }
+}
