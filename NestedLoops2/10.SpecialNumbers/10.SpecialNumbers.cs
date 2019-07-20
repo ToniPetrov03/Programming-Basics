b5351dde29d6787fa7bfb48b6d2f2afa68154f02
Anton Petrov <toniivanov03@gmail.com>
2019-07-20 11:11:33 +0300

    Special numbers

---
+++ NestedLoops2/10.SpecialNumbers/10.SpecialNumbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _10.SpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+
+            for (int i = 1111; i <= 9999; i++)
+            {
+                int n = i;
+                bool isSpecialNumber = true;
+
+                for (int j = 0; j < 4; j++)
+                {
+                    int digit = n % 10;
+
+                    if (digit == 0 || num % digit != 0)
+                    {
+                        isSpecialNumber = false;
+                        break;
+                    }
+
+                    n /= 10;
+                }
+
+                if (isSpecialNumber)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
