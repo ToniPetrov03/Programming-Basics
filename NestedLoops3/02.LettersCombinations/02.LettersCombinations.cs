d36f25be2da5b3b413417a8543f1f32ed0d305ae
Anton Petrov <toniivanov03@gmail.com>
2019-07-24 14:40:57 +0300

    02.Letters combinations and 03.Lucky numbers

---
+++ NestedLoops3/02.LettersCombinations/02.LettersCombinations.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _02.LettersCombinations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char startLetter = char.Parse(Console.ReadLine());
+            char endLetter = char.Parse(Console.ReadLine());
+            char specialLetter = char.Parse(Console.ReadLine());
+
+            int counter = 0;
+
+            for (int firstLetter = startLetter; firstLetter <= endLetter; firstLetter++)
+            {
+                if (firstLetter == specialLetter)
+                {
+                    continue;
+                }
+
+                for (int secondLetter = startLetter; secondLetter <= endLetter; secondLetter++)
+                {
+                    if (secondLetter == specialLetter)
+                    {
+                        continue;
+                    }
+
+                    for (int thirdLetter = startLetter; thirdLetter <= endLetter; thirdLetter++)
+                    {
+                        if (thirdLetter == specialLetter)
+                        {
+                            continue;
+                        }
+
+                        counter++;
+
+                        Console.Write($"{(char)firstLetter}{(char)secondLetter}{(char)thirdLetter} ");
+                    }
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
---
+++ NestedLoops3/03.LuckyNumbers/03.LuckyNumbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _03.LuckyNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int firstNumber = 1; firstNumber <= 9; firstNumber++)
+            {
+                int sumOfFirstTwoNumbers = 0;
+
+                for (int secondNumber = 1; secondNumber <= 9; secondNumber++)
+                {
+                    sumOfFirstTwoNumbers = firstNumber + secondNumber;
+
+                    for (int thirdNumber = 1; thirdNumber <= 9; thirdNumber++)
+                    {
+                        int sumOfSecondTwoNumbers = 0;
+
+                        for (int fourthNumber = 1; fourthNumber <= 9; fourthNumber++)
+                        {
+                            sumOfSecondTwoNumbers = thirdNumber + fourthNumber;
+
+                            if (sumOfFirstTwoNumbers == sumOfSecondTwoNumbers && n % sumOfFirstTwoNumbers == 0)
+                            {
+                                Console.Write($"{firstNumber}{secondNumber}{thirdNumber}{fourthNumber} ");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
