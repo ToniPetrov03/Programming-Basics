db6270bbf8d108ace4e6432030dbb08787b207ab
Anton Petrov <toniivanov03@gmail.com>
2019-07-25 00:08:49 +0300

    Prime pairs

---
+++ NestedLoops3/13.PrimePairs/13.PrimePairs.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _13.PrimePairs
+{
+    class Program
+    {
+        static bool isPrime(int number)
+        {
+            bool result = true;
+
+            if (number == 1)
+            {
+                return false;
+            }
+
+            for (int i = 2; i * i <= number; i++)
+            {
+                if (number % i == 0)
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int startValueOfFirstPair = int.Parse(Console.ReadLine());
+            int startValueOfSecondPair = int.Parse(Console.ReadLine());
+            int differenceOfStartAndEndValueOfFirstPair = int.Parse(Console.ReadLine());
+            int differenceOfStartAndEndValueOfSecondPair = int.Parse(Console.ReadLine());
+
+            for (int firstPair = startValueOfFirstPair; firstPair <= startValueOfFirstPair + differenceOfStartAndEndValueOfFirstPair; firstPair++)
+            {
+                for (int secondPair = startValueOfSecondPair; secondPair <= startValueOfSecondPair + differenceOfStartAndEndValueOfSecondPair; secondPair++)
+                {
+                    if (isPrime(firstPair) && isPrime(secondPair))
+                    {
+                        Console.WriteLine($"{firstPair}{secondPair}");
+                    }
+                }
+            }
+        }
+    }
+}
