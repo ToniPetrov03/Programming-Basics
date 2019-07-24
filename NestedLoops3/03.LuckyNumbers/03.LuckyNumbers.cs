be55bc185312bdaaf88eb2550c6c27e80c55c3d8
Anton Petrov <toniivanov03@gmail.com>
2019-07-24 14:44:26 +0300

    03.Lucky numbers (improved)

--- NestedLoops3/03.LuckyNumbers/03.LuckyNumbers.cs
+++ NestedLoops3/03.LuckyNumbers/03.LuckyNumbers.cs
@@ -10,19 +10,15 @@ namespace _03.LuckyNumbers
 
             for (int firstNumber = 1; firstNumber <= 9; firstNumber++)
             {
-                int sumOfFirstTwoNumbers = 0;
-
                 for (int secondNumber = 1; secondNumber <= 9; secondNumber++)
                 {
-                    sumOfFirstTwoNumbers = firstNumber + secondNumber;
+                    int sumOfFirstTwoNumbers = firstNumber + secondNumber;
 
                     for (int thirdNumber = 1; thirdNumber <= 9; thirdNumber++)
                     {
-                        int sumOfSecondTwoNumbers = 0;
-
                         for (int fourthNumber = 1; fourthNumber <= 9; fourthNumber++)
                         {
-                            sumOfSecondTwoNumbers = thirdNumber + fourthNumber;
+                            int sumOfSecondTwoNumbers = thirdNumber + fourthNumber;
 
                             if (sumOfFirstTwoNumbers == sumOfSecondTwoNumbers && n % sumOfFirstTwoNumbers == 0)
                             {
