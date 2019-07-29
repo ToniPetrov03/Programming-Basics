a44ca9ccc2cf54a5db9ee205a09a1a8df1714d11
Anton Petrov <toniivanov03@gmail.com>
2019-07-29 19:28:08 +0300

    Solve all problems

---
+++ ProgrammingBasicsExam/Problem5/Problem5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Problem5
+{
+    class Problem5
+    {
+        static void Main(string[] args)
+        {
+            int numTickets = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numTickets; i++)
+            {
+                string ticketNumber = Console.ReadLine();
+
+                if (ticketNumber.Length == 4)
+                {
+                    if (ticketNumber[0] >= 'A' && ticketNumber[0] <= 'Z')
+                    {
+                        Console.WriteLine($"Seat decoded: {ticketNumber[0]}{ticketNumber[1]}{ticketNumber[2]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Seat decoded: {ticketNumber[3]}{ticketNumber[1]}{ticketNumber[2]}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"Seat decoded: {ticketNumber[0]}{(int)ticketNumber[1]}");
+                }
+            }
+        }
+    }
+}
---
+++ ProgrammingBasicsExam/Problem6/Problem6.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Problem6
+{
+    class Problem6
+    {
+        static void Main(string[] args)
+        {
+            int numPlayers = int.Parse(Console.ReadLine());
+
+            double totalSum = 0.0;
+            int totalNumSweets = 0;
+
+            for (int i = 0; i < numPlayers; i++)
+            {
+                string name = Console.ReadLine();
+                string sweets = Console.ReadLine();
+                int numCookies = 0;
+                int numCakes = 0;
+                int numWaffles = 0;
+
+                while (sweets != "Stop baking!")
+                {
+                    int numSweets = int.Parse(Console.ReadLine());
+
+                    switch (sweets)
+                    {
+                        case "cookies": totalSum += numSweets * 1.5; numCookies += numSweets; break;
+                        case "cakes": totalSum += numSweets * 7.8; numCakes += numSweets; break;
+                        case "waffles": totalSum += numSweets * 2.3; numWaffles += numSweets; break;
+                    }
+
+                    totalNumSweets += numSweets;
+
+                    sweets = Console.ReadLine();
+                }
+
+                Console.WriteLine($"{name} baked {numCookies} cookies, {numCakes} cakes and {numWaffles} waffles.");
+            }
+
+            Console.WriteLine($"All bakery sold: {totalNumSweets}");
+            Console.WriteLine($"Total sum for charity: {totalSum:F2} lv.");
+        }
+    }
+}
