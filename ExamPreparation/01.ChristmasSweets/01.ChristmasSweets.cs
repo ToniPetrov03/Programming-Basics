7caa887303252f6e8dddb7696fb6bfd7b8350be1
ToniPetrov03 <35171489+ToniPetrov03@users.noreply.github.com>
2019-07-31 09:23:54 +0300

    Exam preparation (#16)

---
+++ ExamPreparation/01.ChristmasSweets/01.ChristmasSweets.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01.ChristmasSweets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceOfBaklavaPerKilogram = double.Parse(Console.ReadLine());
+            double priceOfMuffinsPerKilogram = double.Parse(Console.ReadLine());
+            double stollenKilograms = double.Parse(Console.ReadLine());
+            double candyKilograms = double.Parse(Console.ReadLine());
+            int biscuitsKilograms = int.Parse(Console.ReadLine());
+
+            const double priceOfBiscuitsPerKilogram = 7.5;
+
+            double priceOfStollen = priceOfBaklavaPerKilogram + priceOfBaklavaPerKilogram * 0.6;
+            double sumOfStollen = priceOfStollen * stollenKilograms;
+            double priceOfCandy = priceOfMuffinsPerKilogram + priceOfMuffinsPerKilogram * 0.8;
+            double sumOfCandy = priceOfCandy * candyKilograms;
+            double sumOfBiscuits = priceOfBiscuitsPerKilogram * biscuitsKilograms;
+
+            double bill = sumOfStollen + sumOfCandy + sumOfBiscuits;
+
+            Console.WriteLine($"{bill:F2}");
+        }
+    }
+}
---
+++ ExamPreparation/03.WorldSnookerChampionship/03.WorldSnookerChampionship.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace _03.WorldSnookerChampionship
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stageOfChampionship = Console.ReadLine();
+            string typeOfTicket = Console.ReadLine();
+            int numTickets = int.Parse(Console.ReadLine());
+            char YesOrNo = char.Parse(Console.ReadLine());
+
+            double priceTicket = 0.0;
+
+            switch (stageOfChampionship)
+            {
+                case "Quarter final":
+                    switch (typeOfTicket)
+                    {
+                        case "Standard": priceTicket = 55.5; break;
+                        case "Premium": priceTicket = 105.2; break;
+                        case "VIP": priceTicket = 118.9; break;
+                    }
+                    break;
+
+                case "Semi final":
+                    switch (typeOfTicket)
+                    {
+                        case "Standard": priceTicket = 75.88; break;
+                        case "Premium": priceTicket = 125.22; break;
+                        case "VIP": priceTicket = 300.4; break;
+                    }
+                    break;
+
+                case "Final":
+                    switch (typeOfTicket)
+                    {
+                        case "Standard": priceTicket = 110.1; break;
+                        case "Premium": priceTicket = 160.66; break;
+                        case "VIP": priceTicket = 400; break;
+                    }
+                    break;
+            }
+
+            double totalPrice = priceTicket * numTickets;
+            bool isFreeTicket = false;
+
+            if (totalPrice > 4000)
+            {
+                totalPrice *= 0.75;
+                isFreeTicket = true;
+            }
+            else if (totalPrice > 2500)
+            {
+                totalPrice *= 0.9;
+            }
+
+            if (YesOrNo == 'Y' && !isFreeTicket)
+            {
+                totalPrice += numTickets * 40;
+            }
+
+            Console.WriteLine($"{totalPrice:F2}");
+        }
+    }
+}
